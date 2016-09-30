5150d99ac8479c1b9418704632129578ad76c588
jie.fu <china.eli@gmail.com>
2016-10-01 03:23:44 +0800

    update fuck tc

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var messageRoutes = require('./routes/messages');
 var users = require('./routes/users');
 
 var app = express();
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/messages', messageRoutes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var wxStartHandler = require('../handlers/wxStartHandler.js');
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var wxMessageReplyHandler = require('../handlers/wxMessageReplyHandler.js');
+var xmlparser = require('express-xml-bodyparser');
+var o2x = require('object-to-xml');
 
-router.get('/start', function(req, res, next){
+router.get('/', function(req, res, next){
     wxStartHandler(req.query, function(err, response) {
         if (err) {
             next(err);
         }
         res.send(response);
+    });
+});
+
+router.post('/', xmlparser({trim: true, explicitArray: false}), function(req, res, next){
+    wxMessageReplyHandler(req.body, function(err, response) {
+        if (err) {
+            next(err);
+        }
+        res.set('Content-Type', 'text/xml');
+        //response['?xml version="1.0" encoding="utf-8"?'] = null;
+        var xml_response = "<xml>"+o2x(response)+"</xml>";
+        res.send(xml_response);
     });
 });
 
